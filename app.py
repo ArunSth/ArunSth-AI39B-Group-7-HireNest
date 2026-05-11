a90398af3a4076803a83d0a987ed8411df207f89
ArunSth <arunshresthha@gmail.com>
2026-05-11 22:52:00 +0545

    Setup HireNest Flask backend

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "HireNest is a modern job portal platform that connects job seekers with employers, making hiring faster, smarter, and more efficient through a seamless digital experience."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
