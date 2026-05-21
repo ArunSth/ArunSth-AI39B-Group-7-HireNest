41639297864fb488d0e5c4770cf37e9c157e0701
ArunSth <arunshresthha@gmail.com>
2026-05-21 20:17:01 +0545

    HireNest

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HireNest is a modern job portal platform that connects job seekers with employers, making hiring faster, smarter, and more efficient through a seamless digital experience."
+    return "HireNest is a modern job portal platform that connects job seekers with employers, making hiring faster, smarter, and more efficient through a seamless digital experience...."
 
 
 if __name__ == "__main__":
