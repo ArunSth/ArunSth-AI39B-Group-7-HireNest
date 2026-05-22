5c1091d013314e1ab3328edb9bf02a59925fb539
ArunSth <arunshresthha@gmail.com>
2026-05-22 22:12:09 +0545

    Resolve merge conflict in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ def create_app():
     # Configure Flask to use the `statics` folder for static files
     # and the `templates` folder for templates (both are under app/).
     app = Flask(__name__, static_folder='statics', template_folder='templates')
-    app.secret_key = config.SECRET_KEY
 
     app.register_blueprint(LoginRoutes().login())
     app.register_blueprint(RegistrationRoutes().registration())
